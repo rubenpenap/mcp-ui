1146da24e8823a633d8f5f80dd2df6baf5f75e30
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-10-17 23:02:04 -0300

    refactor: simplify entry viewer route by removing dynamic segment in favor of render data

--- playground/app/routes.ts
+++ playground/app/routes.ts
@@ -4,7 +4,6 @@ export default [
 	index('routes/index.tsx'),
 	route('healthcheck', 'routes/healthcheck.tsx'),
 	route('ui/journal-viewer', 'routes/ui/journal-viewer.tsx'),
-	// 🐨 get rid of the :entryId dynamic segment here (we get the entry data from renderData now)
-	route('ui/entry-viewer/:entryId', 'routes/ui/entry-viewer.tsx'),
+	route('ui/entry-viewer', 'routes/ui/entry-viewer.tsx'),
 	route('/*', 'routes/catch-all.tsx'),
 ] satisfies RouteConfig
